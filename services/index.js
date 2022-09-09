6210db08b6b2f6721db778c4b9989445868b6b7e
Mihir L. Agrawal <mihiragrawal022@gmail.com>
2022-09-09 20:30:07 +0530

    Changed the env variable

--- services/index.js
+++ services/index.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const compileAPI = async(code,language,stdin) => {
   const req = {'code':code,'language':language,'stdin':stdin};
   try{
-    const res = await axios.post(process.env.URL,req);
+    const res = await axios.post(process.env.SERVICE_URL,req);
     return {'stderr':res.data.stderr||'','stdout':res.data.stdout||''};
   }catch(err){
     return {'stderr':'Internal Server Error','stdout':''};
